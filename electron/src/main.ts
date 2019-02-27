61a23a0fcca39797674a779fa3cd4cbd672b2beb
Touhid Mia <hmtm.cse@gmail.com>
2019-02-27 23:38:11 +0600

    Added React

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -5,10 +5,11 @@ import {PathUtil} from "./utils/PathUtil";
 let browserWindow: Electron.BrowserWindow;
 function createWindow() {
     browserWindow = new BrowserWindow({
-        height: 1000,
-        width: 1200,
+        height: 768,
+        width: 1366,
     });
     browserWindow.loadFile(PathUtil.staticResources("index.html"));
+    browserWindow.setMenu(null);
     browserWindow.webContents.openDevTools();
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+module.exports = {
+    entry: './react-app/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'react-bundle.js'
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        plugins: ["transform-class-properties"]
+                    }
+                }
+            }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
+
