25e93765ce93462708c46909e21ea6f3fe2403e0
Touhid Mia <hmtm.cse@gmail.com>
2019-02-28 17:37:18 +0600

    Added RaWysiwygEditor

--- react-app/src/design/tm-main-view.js
+++ react-app/src/design/tm-main-view.js
@@ -22,31 +22,6 @@ export default class TMMainView extends Component {
                 <span className="icon icon-home"></span>
                 connors
               </span>
-
-
-                                    <span className="nav-group-item">
-                <span className="icon icon-light-up"></span>
-                Photon
-                              <span className="nav-group-item">
-                <span className="icon icon-download"></span>
-                Downloads
-                                                              <span className="nav-group-item">
-                <span className="icon icon-download"></span>
-                Downloads
-                                                                                              <span
-                                                                                                  className="nav-group-item">
-                <span className="icon icon-download"></span>
-                Downloads
-              </span>
-              </span>
-              </span>
-              </span>
-
-
-                                    <span className="nav-group-item">
-                <span className="icon icon-download"></span>
-                Downloads
-              </span>
                                     <span className="nav-group-item">
                 <span className="icon icon-folder"></span>
                 Documents
@@ -67,126 +42,9 @@ export default class TMMainView extends Component {
                             </div>
 
                             <div className="pane">
-                                <table className="table-striped">
-                                    <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Kind</th>
-                                        <th>Date Modified</th>
-                                        <th>Author</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>bars.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>base.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>button-groups.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>buttons.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>docs.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>forms.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>grid.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>icons.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>images.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>lists.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>mixins.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>navs.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>normalize.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>photon.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>tables.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>tabs.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>utilities.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    <tr>
-                                        <td>variables.scss</td>
-                                        <td>Document</td>
-                                        <td>Oct 13, 2015</td>
-                                        <td>connors</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+                                <RaWysiwygEditor/>
+
+
                             </div>
                         </div>
                     </div>
